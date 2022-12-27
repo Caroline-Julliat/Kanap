8509c3d2d10d3a9f834fc56774a6f68ff1b03a16
Caroline Julliat <carolinejulliat@gmail.com>
2022-12-27 23:18:26 +0100

    function fetchProducts on product page

--- front/js/product.js
+++ front/js/product.js
@@ -5,3 +5,14 @@ const productId = url.searchParams.get("id")
 
 let allProducts = []
 let pageProduct = []
+
+//Fonction de récuppération des données
+async function fetchProducts() {
+    await fetch("http://localhost:3000/api/products")
+      .then((res) => res.json())
+      .then((data) => (allProducts = data))
+  
+    pageProduct = allProducts.filter((product) => product._id == productId)
+    console.log("pageProduct =")
+    console.log(pageProduct)
+  }
