bc9712b2f1a82a8bbc92dc8ef6322928d308c3e0
Caroline Julliat <carolinejulliat@gmail.com>
2022-12-27 23:56:48 +0100

    display product on product page

--- front/js/product.js
+++ front/js/product.js
@@ -8,11 +8,40 @@ let pageProduct = []
 
 //Fonction de récuppération des données
 async function fetchProducts() {
-    await fetch("http://localhost:3000/api/products")
-      .then((res) => res.json())
-      .then((data) => (allProducts = data))
-  
-    pageProduct = allProducts.filter((product) => product._id == productId)
-    console.log("pageProduct =")
-    console.log(pageProduct)
+  await fetch("http://localhost:3000/api/products")
+    .then((res) => res.json())
+    .then((data) => (allProducts = data))
+
+  pageProduct = allProducts.filter((product) => product._id == productId)
+  console.log("pageProduct =")
+  console.log(pageProduct)
+}
+
+// Fonction d'affichage des données
+async function displayProduct() {
+  await fetchProducts()
+
+  const itemImgContainer = document.querySelector(".item__img")
+  const itemImg = document.createElement("img")
+  const itemName = document.getElementById("title")
+  const itemPrice = document.getElementById("price")
+  const itemDescription = document.getElementById("description")
+  const itemColors = document.getElementById("colors")
+
+  itemImg.src = pageProduct[0].imageUrl
+  itemImg.alt = pageProduct[0].altTxt
+  itemImgContainer.appendChild(itemImg)
+  itemName.textContent = pageProduct[0].name
+  itemPrice.textContent = pageProduct[0].price
+  itemDescription.textContent = pageProduct[0].description
+
+  for (const color of pageProduct[0].colors) {
+    let colorOption = document.createElement("option")
+    colorOption.setAttribute("value", color)
+    colorOption.textContent = color
+    itemColors.appendChild(colorOption)
   }
+}
+
+displayProduct()
+
