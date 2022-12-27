48f02f779c3379f09f80e639fcb792e6b852a8f2
Caroline Julliat <carolinejulliat@gmail.com>
2022-12-27 23:13:04 +0100

    display products on script page

--- front/js/script.js
+++ front/js/script.js
@@ -9,3 +9,35 @@ async function fetchProducts() {
 
   console.log(allProducts)
 }
+
+//Affichage des produits
+async function displayProducts() {
+  await fetchProducts()
+
+  for (let i = 0; i < allProducts.length; i++) {
+    const lien = document.createElement("a")
+    resultContainer.appendChild(lien)
+    lien.href = `./product.html?id=${allProducts[i]._id}`
+
+    const article = document.createElement("article")
+    lien.appendChild(article)
+
+    const img = document.createElement("img")
+    img.src = allProducts[i].imageUrl
+    img.alt = allProducts[i].altTxt
+
+    const h3 = document.createElement("h3")
+    h3.classList.add("productName")
+    h3.textContent = allProducts[i].name
+
+    const p = document.createElement("p")
+    p.classList.add("productDescription")
+    p.textContent = allProducts[i].description
+
+    article.appendChild(img)
+    article.appendChild(h3)
+    article.appendChild(p)
+  }
+}
+
+displayProducts()
